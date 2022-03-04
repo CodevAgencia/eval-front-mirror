e2dd40360b6322e58801d9caf1cd388168f47b3c
Johnny Pacheco <johnny.pacheco@treinta.co>
2022-03-04 11:03:10 -0500

    Logout button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
+import { IconButton } from '@mui/material';
+import { Logout } from '@mui/icons-material';
+
 import { useAuth } from '../../hooks';
 import Logo from '../../images/Logo-v2.png';
 
 const Header = () => {
-  const { loggedIn } = useAuth();
+  const { loggedIn, logout } = useAuth();
 
   return loggedIn ? (
     <div className="w-full h-44 flex items-center sticky bg-white z-10 top-0 py-4 border-b-2 border-cyan-400">
-      <div className="flex flex-col justify-center h-32 mx-auto" style={{ width: '90%' }}>
+      <div className="flex flex-row justify-center h-32 mx-auto" style={{ width: '90%' }}>
+        <div />
         <div className="w-full flex items-center justify-center space-x-8">
           <img
             src={Logo}
@@ -15,6 +19,11 @@ const Header = () => {
             style={{ maxWidth: 'max-content' }}
           />
           <h4 className="font-bold text-black text-10 md:text-11">Zona Innfinita</h4>
+        </div>
+        <div>
+          <IconButton onClick={logout} edge="end">
+            <Logout />
+          </IconButton>
         </div>
       </div>
     </div>
