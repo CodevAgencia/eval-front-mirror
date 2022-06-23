57bc98cbbee7244b6250d7967ddb0da6cadcbced
Johnny Pacheco <johnny.pacheco@treinta.co>
2022-06-23 02:13:50 -0500

    Add back button on results

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,9 +1,16 @@
+import { useHistory } from 'react-router';
 import { Typography } from '@material-ui/core';
 
 import { useAuth } from '../hooks';
+import { GROUPS_FORM } from '../common/constants';
 
 const Results = () => {
   const { logout } = useAuth();
+  const { push } = useHistory();
+
+  const goBack = () => {
+    push(`/formulario/${GROUPS_FORM[GROUPS_FORM.length - 1]}`);
+  };
 
   // Return message with "La información de tu startup ha sido enviada con éxito"
   return (
@@ -18,14 +25,25 @@ const Results = () => {
       <br />
       <br />
       <br />
-      <br />
-      <button
-        type="button"
-        onClick={logout}
-        className="w-full bg-cyan-400 hover:bg-cyan-600 text-white font-bold py-2 px-4 rounded"
-      >
-        Salir
-      </button>
+      <div className="flex flex-row">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            goBack();
+          }}
+          type="button"
+          className="w-full variant:outlined bg-blue-50 hover:bg-cyan-600 hover:text-white text-cyan-400 font-bold mx-4 py-2 px-4 rounded"
+        >
+          Atras
+        </button>
+        <button
+          type="button"
+          onClick={logout}
+          className="w-full bg-cyan-400 hover:bg-cyan-600 text-white font-bold mx-4 py-2 px-4 rounded"
+        >
+          Salir
+        </button>
+      </div>
     </div>
   );
 };
